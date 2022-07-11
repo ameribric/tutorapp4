a464acf4e34e1a7550f40abaaf09160034a13c9f
Amer Ibric <ameribric@gmail.com>
2022-07-11 13:43:29 -0400

    Added the ability to update time of meeting

--- client/src/components/MeetingList.js
+++ client/src/components/MeetingList.js
@@ -1,34 +1,45 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 import "./MeetingList.css"
 
-function MeetingList({ meetings, onDeleteMeeting, id}) {
-  // const [updatedTime, setUpdatedTime] = useState("");
-
+function MeetingList({ meetings, onDeleteMeeting, onUpdateMeeting, id }) {
+  const [updatedTime, setUpdatedTime] = useState("");
 
   function handleDeleteClick(id) {
     return () => {
+      fetch(`meetings/${id}`, {
+        method: "DELETE",
+      });
+      onDeleteMeeting(id);
+    };
+  }
+
+  function handleUpdateTimeClick(id) {
+    return () => {
+      fetch(`meetings/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ time: updatedTime }),
+      });
+      onUpdateMeeting(id)
+    }
+  }
+
+  function handleTimeFormSubmit(e, id) {
+    e.preventDefault();
     fetch(`meetings/${id}`, {
-      method: "DELETE",
-    });
-    onDeleteMeeting(id);
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ time: updatedTime }),
+    })
+      .then((r) => r.json())
+      .then((updatedMeeting) => {
+        onUpdateMeeting(updatedMeeting);
+      });
   }
-  }
-
-
-  // function handleTimeFormSubmit(e) {
-  //   e.preventDefault();
-  //   fetch(`meetings/${meeting.id}`, {
-  //     method: "PATCH",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({ time: updatedTime }),
-  //   })
-  //     .then((r) => r.json())
-  //     .then((updatedMeeting) => {
-  //       onUpdateMeeting(updatedMeeting);
-  //     });
-  // }
 
   return (
     <ul className="meeting-list-header">
@@ -45,15 +56,15 @@ function MeetingList({ meetings, onDeleteMeeting, id}) {
             <h4>{meeting.tutor_id}</h4>
 
             <button onClick={handleDeleteClick(meeting.id)}>Delete</button>
-            {/* <form onSubmit={handleTimeFormSubmit}>
+            <form onSubmit={handleTimeFormSubmit}>
         <input
           type="text"
           placeholder="New time..."
           value={updatedTime}
           onChange={(e) => setUpdatedTime(e.target.value)}
         />
-        <button type="submit">Update Time</button>
-      </form> */}
+        <button type="submit" onClick={handleUpdateTimeClick(meeting.id)}>Update Time</button>
+      </form>
           </li>
         );
       })}
